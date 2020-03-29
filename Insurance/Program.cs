eb517184b0960e394c54f642c5212d592818dd62
Bianca <bmstorm13@users.noreply.github.com>
2020-03-29 15:43:38 -0700

    insurance assignment
    step 101 c#

---
+++ Insurance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you everf had a DUI? true or false?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = (age > 15 && dui == false && tickets <= 3);
+            Console.WriteLine("Qualified? " + qualified);
+            Console.ReadLine();
+        }
+    }
+}
