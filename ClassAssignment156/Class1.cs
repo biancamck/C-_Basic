5cacbe2c6f66b8733e9941db0d760b20cf90ca22
Bianca <bmstorm13@users.noreply.github.com>
2020-04-07 14:49:48 -0700

    C# 156

---
+++ ClassAssignment156/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment156
+{
+    public class Number
+    {
+        public int NumberAdd(int x)
+        {
+            int sum = x + 100;
+            return sum;
+           
+        }
+
+        public int NumberMult(int y)
+        {
+            int multiplication = y * 5;
+            return multiplication;
+            
+        }
+
+        public int NumberSub(int z)
+        {
+            int subtraction = z - 3;
+            return subtraction;
+           
+        }
+
+    }
+
+}
---
+++ ClassAssignment156/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, lets do some math operations!");
+            Console.WriteLine("Enter a whole number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            Number newnumber = new Number();
+            int result1 = newnumber.NumberAdd(userNumber);
+            Console.WriteLine("Your number plus 100 = " + result1);
+            Console.ReadLine();
+
+            Number newnumber2 = new Number();
+            int result2 = newnumber2.NumberMult(userNumber);
+            Console.WriteLine("Your number multiplied byt 5 = " + result2);
+            Console.ReadLine();
+
+            Number newnumber3 = new Number();
+            int result3 = newnumber3.NumberSub(userNumber);
+            Console.WriteLine("Your number minus 3 = " + result3);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
