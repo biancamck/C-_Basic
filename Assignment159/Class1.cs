b29a96fabf06fbae808731fd8b2b0d61fb1b3a54
Bianca <bmstorm13@users.noreply.github.com>
2020-04-08 18:38:17 -0700

    C# 159

---
+++ Assignment159/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment159
+{
+    public class Number
+    {
+        public int NumberAdd(int x)
+        {
+            int sum = x + 100;
+            return sum;
+        }
+
+        public decimal NumberMult(decimal y)
+        {
+            decimal multiplication = y * 10;
+            return multiplication;
+        }
+
+        public int NumberSub(int z)
+        {
+            int subtraction = z - 3;
+            return subtraction;
+
+        }
+    }
+}
+
---
+++ Assignment159/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number newnumber = new Number();
+            int y = 5;
+            int result1 = newnumber.NumberAdd(y);
+            Console.WriteLine("5 + 100 = " + result1);
+            Console.ReadLine();
+
+            Number newnumber2 = new Number();
+            decimal x = 1.5m;
+            decimal result2 = newnumber2.NumberMult(x);
+            Console.WriteLine("1.5m x 10 = " + result2);
+            Console.ReadLine();
+
+            Console.WriteLine("enter a number");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Number newnumber3 = new Number();
+            int result3 = newnumber3.NumberSub(z);
+            Console.WriteLine("Your number " + z + " minus 3 = " + result3);
+            Console.ReadLine();
+        }
+    }
+}
