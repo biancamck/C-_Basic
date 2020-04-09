629c5445399eaae2d7042ad57b977f6fff9ac398
Bianca <bmstorm13@users.noreply.github.com>
2020-04-08 19:17:50 -0700

    c# 160

---
+++ Assignment160/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment160
+{
+    public class Number
+    {
+        public int NumberAdd(int x, int y = 1)
+        {
+            int sum = x + y + 100;
+            return sum;
+
+        }
+
+        public int x { get; set; }       
+        public int y { get; set; }
+    }
+}
---
+++ Assignment160/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number newnumber = new Number();
+            int y;
+            int result;
+
+            Console.WriteLine("type a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("optional enter a second number");
+            
+            if (int.TryParse(Console.ReadLine(), out y))
+            {
+                result = newnumber.NumberAdd(x, y);
+            }
+            
+            else
+            {
+                result = newnumber.NumberAdd(x);
+            }
+           
+            Console.WriteLine("the sum of all numbers = " + result);
+            Console.ReadLine();
+        }
+    }
+}
