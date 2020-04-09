3b57fac0bc2a0999c087be618a938ecfec26c5c1
Bianca <bmstorm13@users.noreply.github.com>
2020-04-08 20:10:14 -0700

    C# 161

---
+++ Assignment161/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment161
+{
+    public class Number
+    {
+        public void NumberAdd(int x, int y)
+        {
+            int sum = x + 100;
+            
+            Console.WriteLine(y);
+            
+        }
+        
+    }
+}
+
+       
---
+++ Assignment161/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number newnumber = new Number();
+            int a = 10;
+            int b = 5;
+            newnumber.NumberAdd(a,b);
+            Console.ReadLine();
+
+            Number newnumber2 = new Number();
+            newnumber.NumberAdd(y: 9, x: 3);
+            Console.ReadLine();
+        }
+        
+    }
+}
