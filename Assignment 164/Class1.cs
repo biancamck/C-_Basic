e7450d89ca86c79998f9adee8bc0f438b2852d87
Bianca <bmstorm13@users.noreply.github.com>
2020-04-14 10:30:17 -0700

    C# 164

---
+++ Assignment 164/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_164
+{
+    class Number        //class - 1
+    {
+        public void Divide(int x)
+        {
+            int result = x / 2;
+            Console.WriteLine("The result = " + result);
+        }
+    }
+}
---
+++ Assignment 164/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Assignment_164
+{   
+    static class Program        //static class - 6
+    {
+        static void Main(string[] args)
+        {
+            Number newnumber = new Number();        //instantiate class - 2
+            Console.WriteLine("enter a number");
+            int x = Convert.ToInt32(Console.ReadLine());        //user enters number, perform funcrion - 3
+            newnumber.Divide(x);
+            Console.ReadLine();
+        }
+
+       
+        
+        // out params - 4
+
+        public static void Addition(out int p, out int q)
+        {
+            p = 30;
+            q = 40;
+            p += p;
+            q += q;
+            Console.WriteLine("the solution for p is " + p);
+            Console.WriteLine("the solution for q is " + q);
+            Console.ReadLine();
+        }
+
+
+
+
+        
+        /// method overloading - 5
+        
+     
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main2(string[] args)
+        {
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+        }
+    }
+
+}
+
