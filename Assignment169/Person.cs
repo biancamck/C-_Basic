4a7dc7fddb2be11f898e7d278119cf0ea57005bf
Bianca <bmstorm13@users.noreply.github.com>
2020-04-14 13:01:44 -0700

    assignment 169 C#

---
+++ Assignment169/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment169
+{
+    public class Person
+    {
+       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName(string FirstName, string LastName)
+        {
+            FirstName = "B";
+            LastName = "M";
+
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Assignment169/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            
+            Console.WriteLine("Name: " + employee.FirstName + " " + employee.LastName);
+            Console.ReadLine();
+            }
+    }
+}
