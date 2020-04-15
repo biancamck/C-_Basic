9a465b2d9f91fb425e2e74d32f5fc05bbbac9d22
Bianca <bmstorm13@users.noreply.github.com>
2020-04-14 17:07:19 -0700

    C# 176

---
+++ Assignment176/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            //Person.SayName(employee);
+            Console.WriteLine("Name: " + employee.FirstName + " " + employee.LastName);
+            Console.ReadLine();
+        }
+    }
+}
