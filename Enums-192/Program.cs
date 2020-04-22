2697a723a04872f9b38d96c4b92b7f770e9b6be9
Bianca <bmstorm13@users.noreply.github.com>
2020-04-22 11:08:33 -0700

    C# 192

---
+++ Enums-192/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_192
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a day of the week.");
+                string userDay = Console.ReadLine();
+                Days myDay = (Days)Enum.Parse(typeof(Days), userDay, ignoreCase:true);
+                Console.WriteLine("Your chosen day, " + myDay + " is a day of the week");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Enums-192/Week.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_192
+{
+    class Week
+    {
+        public string Days { get; set; }
+    }
+
+    public enum Days
+    { 
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
+
+
+
+
+
+
+public enum Suit
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
