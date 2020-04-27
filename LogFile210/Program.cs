89a23213d697fd4577204402ed81b79752449306
Bianca <bmstorm13@users.noreply.github.com>
2020-04-27 14:19:32 -0700

    C# 210

---
+++ LogFile210/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogFile210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\repository\C#\log210.txt", number);
+            string text = File.ReadAllText(@"C:\repository\C#\log210.txt");
+            Console.WriteLine("from the file..." + text);
+            Console.ReadLine();
+        }
+    }
+}
