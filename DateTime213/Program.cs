6d3534a2b35067ee9163feebd87ccb2d332bc807
Bianca <bmstorm13@users.noreply.github.com>
2020-04-27 14:49:16 -0700

    C#  213

---
+++ DateTime213/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The time is " + DateTime.Now);
+            Console.ReadLine();
+            Console.WriteLine("Enter a number");
+            int X = Convert.ToInt32(Console.ReadLine());
+            DateTime FutureTime = DateTime.Now.AddHours(X);
+            Console.WriteLine("In {0} hours, the time will be {1}", X,FutureTime);
+            Console.ReadLine();
+            
+           
+        }
+    }
+}
