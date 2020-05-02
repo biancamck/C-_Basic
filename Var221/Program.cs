59671ad43103a2e7df3ac5341f5987e772b70136
Bianca <bmstorm13@users.noreply.github.com>
2020-05-02 15:52:26 -0700

    C# 221

---
+++ Var221/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var221
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            const string hello = "Hey there,";
+            var name = "Bianca";
+            Console.WriteLine(hello + name);
+            Console.ReadLine();
+        }
+    }
+}
